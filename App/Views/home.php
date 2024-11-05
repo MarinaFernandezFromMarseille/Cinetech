2c3c6194f5466e28f4704e0effa96564597a758a
MarinaFernandezFromMarseille <168725135+MarinaFernandezFromMarseille@users.noreply.github.com>
2024-11-05 16:56:36 +0100

    MVC

--- App/Views/home.php
+++ App/Views/home.php
@@ -4,40 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cinetech</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            text-align: center;
-        }
-
-        h1 {
-            font-size: 3em;
-            color: #333;
-        }
-
-        p {
-            font-size: 1.2em;
-            color: #666;
-        }
-
-        nav {
-            background-color: #333;
-            padding: 1em;
-        }
-
-        nav a {
-            color: #fff;
-            text-decoration: none;
-            margin: 0 1em;
-        }
-
-    </style>
+    <link rel="stylesheet" href="Assets/CSS/home.css?v=<?php echo time(); ?>">
 </head>
 <body>
     <nav>
-        <a href="https://github.com/pinkydev/cinetech">GitHub</a>
-        <a href="https://pinkydev.github.io/cinetech">Blog</a>
-        <a href="https://twitter.com/pinkydev">Twitter</a>
+        <a href="https://github.com/MarinaFernandezFromMarseille/Cinetech">GitHub</a>
+        <a href="https://82.165.185.52:8443/smb/web/view">Plesk</a>
+<form class = "search-form" action="" method="get">
+    <input type="text" name="query" placeholder="Search..." required>
+</form>
     </nav>
     <h1>Welcome to Cinetech</h1>
     <p>This is a place for all things film and technology.</p>
